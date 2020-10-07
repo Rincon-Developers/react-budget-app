1a4313eab72cceeb2b12055c7cd8b571280d7cd3
Julian Rincon <julian.vera@ceiba.com.co>
2020-10-06 22:16:42 -0500

    Initial structure of project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>BUDGET APP</h3>
+      <BudgetForm/>
+      <ExpensesForm/>
+      <DashBoard/>      
     </div>
+  );
+}
+
+function BudgetForm() {
+  return (
+    <div>
+      <form>
+        <label>
+          Please enter your budget
+        </label>
+        <input type="number"></input>
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
+}
+
+function ExpensesForm() {
+  return (
+    <div>
+      <form>
+        <label>
+          Please enter your expense
+        </label>
+        <input type="text"></input>
+        <label>
+          Please enter your expense amount
+        </label>
+        <input type="number"></input>
+        <button type="submit">Add Expense</button>
+      </form>
+    </div>
+  );
+}
+
+function DashBoard() {
+  return (
+    <div><h2>DashBoard Section</h2></div>
   );
 }
 
